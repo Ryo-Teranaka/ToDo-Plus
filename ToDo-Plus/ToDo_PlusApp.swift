25e802230f1da861f19ab43899cbf015d1234cae
Ryo <xxxpppd7@gmail.com>
2022-12-24 18:32:29 +0900

    Initial Commit

---
+++ ToDo-Plus/ToDo_PlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_PlusApp.swift
+//  ToDo-Plus
+//
+//  Created by TERANAKA RYO on 2022/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_PlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
