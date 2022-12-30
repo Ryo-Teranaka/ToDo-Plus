8b79d27a444b78a94be52e95aa324a8fa5a5473c
Ryo <xxxpppd7@gmail.com>
2022-12-30 14:05:21 +0900

    [Add] 開発用基本ファイル追加

---
+++ ToDo-Plus/SubTaskListView.swift
@@ -0,0 +1,20 @@
+//
+//  SubTaskListView.swift
+//  ToDo-Plus
+//
+//  Created by TERANAKA RYO on 2022/12/25.
+//
+
+import SwiftUI
+
+struct SubTaskListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SubTaskListView_Previews: PreviewProvider {
+    static var previews: some View {
+        SubTaskListView()
+    }
+}
---
+++ ToDo-Plus/TaskListView.swift
@@ -0,0 +1,20 @@
+//
+//  TaskListView.swift
+//  ToDo-Plus
+//
+//  Created by TERANAKA RYO on 2022/12/24.
+//
+
+import SwiftUI
+
+struct TaskListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TaskListView_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskListView()
+    }
+}
